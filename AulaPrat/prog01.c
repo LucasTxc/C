2d0c6be2fdf0323de0eef619933492cea98a5cd8
Lucas Teixeira <53348834+LucasTxc@users.noreply.github.com>
2022-02-19 21:55:57 -0300

    Aula 18/02
    Preparando vscode

---
+++ AulaPrat/prog01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    printf("Hello World");
+
+    int i;
+    int v[500];
+
+    for(i=0; i < 500; i++){
+
+        printf("%d \t", i);
+    }
+
+
+    return 0;
+}
